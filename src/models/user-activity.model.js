f99c203b18d8a67dd1f2858842f61e6488232822
halonthe <halonthe@gmail.com>
2025-08-06 09:45:32 +0700

    feat: create user-activity model

---
+++ src/models/user-activity.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const userActivitySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    action: {
+      type: String,
+      required: true,
+      enum: ["SIGNIN", "SIGNUP", "SIGNOUT", "EMAIL_VERIFICATION"],
+    },
+    deviceInfo: {
+      browser: {
+        name: String,
+        version: String,
+      },
+      os: {
+        name: String,
+        version: String,
+      },
+      device: {
+        type: String, // mobile, tablet, desktop
+        brand: String,
+        model: String,
+      },
+    },
+    location: {
+      ip: String,
+      city: String,
+      country: String,
+      timezone: String,
+    },
+    sessionData: {
+      userAgent: String,
+      timestamp: {
+        type: Date,
+        default: Date.now(),
+      },
+      duration: Number, // in second
+    },
+    status: {
+      type: String,
+      enum: ["SUCCESS", "FAILED", "PENDING"],
+      required: true,
+    },
+    details: String,
+  },
+  { timestamps: true }
+);
+
+userActivitySchema.index({ user: 1, "sessionData.timestamp": -1 });
+userActivitySchema.index({ "deviceInfo.device.type": 1 });
+userActivitySchema.index({ action: 1 });
+
+// Prevent model overwrite in dev mode
+const UserActivity =
+  mongoose.models.UserActivity ||
+  mongoose.model("UserActivity", userActivitySchema);
+export default UserActivity;
