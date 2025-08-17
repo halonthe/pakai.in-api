f9055b896ccb9d51b1233fb0e3631b263f0d2687
halonthe <halonthe@gmail.com>
2025-08-17 16:09:13 +0700

    fix: fix transporter (google credentials)

--- src/configs/email.js
+++ src/configs/email.js
@@ -1,37 +1,40 @@
-// import { google } from "googleapis";
+import { google } from "googleapis";
 import nodemailer from "nodemailer";
 import "dotenv/config";
 
 // 1 - setup google credentials
-// const oAuth2Client = new google.auth.OAuth2(
-//   process.env.GOOGLE_CLIENT_ID,
-//   process.env.GOOGLE_CLIENT_SECRET,
-//   process.env.GOOGLE_REDIRECT_URI
-// );
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI,
+  process.env.GOOGLE_AUTH_CODE
+);
 
-// oAuth2Client.setCredentials({
-//   refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-// });
+oAuth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+});
 
-// async function getToken() {
-//   try {
-//     const { token } = await oAuth2Client.getAccessToken();
-//     return token;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+async function getToken() {
+  try {
+    const { token } = await oAuth2Client.getAccessToken();
+    return token;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // 2 - setup nodemailer
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     type: "OAuth2",
     user: process.env.SMTP_USER,
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    // accessToken: getToken(),
+    accessToken: getToken(),
   },
 });
 
